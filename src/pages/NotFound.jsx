c2bdcf83380c8f299165451fa92b22abef3752b4
EUi SEON CHI <bbcd6222@gmail.com>
2022-01-25 01:36:35 +0900

    [ADD] Pages and routes setup router-dom v6, not found pages styling with tailwindCSS and daisyui for the Link button

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <div className="hero ">
+      <div className="text-center hero-cotent">
+        <div className="max-wlg">
+          <h1 className="text-8xl font-bold mb-8">Oops!</h1>
+          <p className="text-5xl mb-8">404 - Page not found !</p>
+          <Link to="/" className="btn btn-primary btn-lg">
+            <FaHome className="mr-2" />
+            Back To Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
